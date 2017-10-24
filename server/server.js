d2cf6afea2e39082cbb91d7e4e48a77a243300a9
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2017-10-24 23:09:33 +0100

    Server running

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("hello")
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server listening on port ', port);
+});
