00cf13553d32b9a318962cdcf2e6e7fe79be9563
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2017-10-26 08:58:19 +0100

    eslint format

--- server/test/test.js
+++ server/test/test.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const chai = require('chai');
 const expect = require('chai').expect;
@@ -7,54 +7,44 @@ chai.use(require('chai-http'));
 
 const app = require('../server.js');
 
-describe('API endpoint /api/recipes', function() {
-
-  before(function() {
+describe('API endpoint /api/recipes', () => {
+  before(() => {
 
   });
 
-  after(function() {
+  after(() => {
 
   });
 
   // GET - Return all recipes
-  it('should return all recipes', function() {
-    return chai.request(app)
-      .get('/api/recipes')
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-      });
-  });
+  it('should return all recipes', () => chai.request(app)
+    .get('/api/recipes')
+    .then((res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+    }));
 
   // POST - Post a recipe
-  it('should return all recipes', function() {
-    return chai.request(app)
-      .post('/api/recipes')
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-      });
-  });
+  it('should return all recipes', () => chai.request(app)
+    .post('/api/recipes')
+    .then((res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+    }));
 
   // PUT - modify a recipe
-  it('should return all recipes', function() {
-    return chai.request(app)
-      .put('/api/recipes/4')
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-      });
-  })
+  it('should return all recipes', () => chai.request(app)
+    .put('/api/recipes/4')
+    .then((res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+    }));
 
   // POST - post a review for recipe
-  it('should return all recipes', function() {
-    return chai.request(app)
-      .put('/api/recipes/4')
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-      });
-  })
-  
+  it('should return all recipes', () => chai.request(app)
+    .put('/api/recipes/4')
+    .then((res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+    }));
 });
